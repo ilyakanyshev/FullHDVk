4847f2b6ba356f4dd9444507f534b76f71c16ee3
Ilya Kanyshev <26299517+ilyakanyshev@users.noreply.github.com>
2017-08-31 10:32:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.onload = function () {
+setInterval(function(){
+if (document.getElementById('wide_column')) {
+var width = document.getElementById('wide_column').offsetWidth;
+}
+if (document.getElementById('page_body')) {
+var width = document.getElementById('page_body').offsetWidth;
+}
+if (document.getElementById('im_dialogs')) {
+var width = document.getElementById('im_dialogs').offsetWidth;
+}
+$("#audio_layer_tt").width(width);
 var width = document.body.offsetWidth; 
 document.getElementsByClassName('page_cover crisp_image _page_cover');
 if (width>1292) {
@@ -8,6 +19,10 @@ document.getElementById('page_header').style.width ='calc(100% - 284px)';
 if (document.getElementById('dev_top_nav')) {
 document.getElementById('dev_top_nav').style.width ='calc(100% - 284px)'; 
 }
+var width1 = document.body.offsetWidth;
+var width2 = document.getElementById('page_header').offsetWidth;
+$('.im-right-menu').css({right: (width1-width2)/2+"px"});
+
 if (document.getElementById('page_layout')) {
 document.getElementById('page_layout').style.width = 'calc(100% - 284px)'; 
 }
@@ -20,6 +35,7 @@ $("div.page_cover.crisp_image._page_cover").height((200*width/795)+'px');
 if (document.getElementById('page_header')) {
 document.getElementById('page_header').style.width = '960px';
 } 
+$('.im-right-menu').css({right: auto});
 if (document.getElementById('dev_top_nav')) {
 document.getElementById('dev_top_nav').style.width ='960px'; 
 }
@@ -32,21 +48,8 @@ document.getElementById('page_body').style.width = '795px';
 var width = $("div.page_cover.crisp_image._page_cover").width();
 $("div.page_cover.crisp_image._page_cover").height('200px');
 } 
-//var width = $('div.ui_search_input_block').width();
 $("#audio_layer_tt").width(width);
-//$(".page_post_sized_thumbs.clear_fix").marginLeft('50%');
 $("div.page_post_sized_thumbs .clear_fix").width("100%");
 $('.audio_section.audio_w_covers._audio_section._audio_section__current audio_section__current').width('calc(100% - 44px)');
-};
-setInterval(function(){
-if (document.getElementById('wide_column')) {
-var width = document.getElementById('wide_column').offsetWidth;
-}
-if (document.getElementById('page_body')) {
-var width = document.getElementById('page_body').offsetWidth;
-}
-if (document.getElementById('im_dialogs')) {
-var width = document.getElementById('im_dialogs').offsetWidth;
-}
-$("#audio_layer_tt").width(width);
 },100);
+};
